bbe754116a7c4e0b1918e31c1f512069f0d67cd3
Vytautas Šablinskas <vytsab2@ktu.lt>
2023-12-26 20:00:30 +0200

    Add starting files

---
+++ src/main/java/com/vytsablinskas/flavorfare/FlavorFareApplication.java
@@ -0,0 +1,13 @@
+package com.vytsablinskas.flavorfare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavorFareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlavorFareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vytsablinskas/flavorfare/FlavorfareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vytsablinskas.flavorfare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavorFareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
