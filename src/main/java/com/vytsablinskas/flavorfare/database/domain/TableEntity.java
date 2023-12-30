96ff393874f69255d0a09e6dc1e213606fe347fe
Vytautas Šablinskas <vytsab2@ktu.lt>
2023-12-31 00:51:18 +0200

    Add table entity

---
+++ src/main/java/com/vytsablinskas/flavorfare/database/domain/TableEntity.java
@@ -0,0 +1,25 @@
+package com.vytsablinskas.flavorfare.database.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="tables")
+public class TableEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    private Integer size;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id")
+    private RestaurantEntity restaurant;
+}
