ab99d74033016457eba44c42aa06886e48fcd0bb
Vytautas Šablinskas <vytsab2@ktu.lt>
2023-12-31 17:55:50 +0200

    Create and test table controller

--- src/main/java/com/vytsablinskas/flavorfare/api/controllers/TableController.java
+++ src/main/java/com/vytsablinskas/flavorfare/api/controllers/TableController.java
@@ -4,6 +4,7 @@ import com.vytsablinskas.flavorfare.business.services.interfaces.TableService;
 import com.vytsablinskas.flavorfare.shared.constants.Prefixes;
 import com.vytsablinskas.flavorfare.shared.dtos.table.AddTableDto;
 import com.vytsablinskas.flavorfare.shared.dtos.table.TableDto;
+import com.vytsablinskas.flavorfare.shared.dtos.table.UpdateTableDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = Prefixes.API_V1)
 public class TableController {
-    private TableService tableService;
+    private final TableService tableService;
 
     public TableController(TableService tableService) {
         this.tableService = tableService;
@@ -21,11 +22,30 @@ public class TableController {
     @GetMapping(path = "restaurants/{restaurantId}/tables")
     @ResponseStatus(HttpStatus.OK)
     public List<TableDto> getTables(@PathVariable Integer restaurantId) {
-        return null;
+        return tableService.getTables(restaurantId);
+    }
+
+    @GetMapping(path = "restaurants/{restaurantId}/tables/{tableId}")
+    @ResponseStatus(HttpStatus.OK)
+    public TableDto getTable(@PathVariable Integer restaurantId, @PathVariable Integer tableId) {
+        return tableService.getTable(restaurantId, tableId);
     }
 
     @PostMapping(path = "restaurants/{restaurantId}/tables")
+    @ResponseStatus(HttpStatus.CREATED)
     public TableDto addTable(@PathVariable Integer restaurantId, @RequestBody AddTableDto addTableDto) {
         return tableService.addTable(restaurantId, addTableDto);
+    }
+
+    @PutMapping(path = "restaurants/{restaurantId}/tables/{tableId}")
+    @ResponseStatus(HttpStatus.OK)
+    public TableDto updateTable(@PathVariable Integer restaurantId, @PathVariable Integer tableId, @RequestBody UpdateTableDto updateTableDto) {
+        return tableService.updateTable(restaurantId, tableId, updateTableDto);
+    }
+
+    @DeleteMapping(path = "restaurants/{restaurantId}/tables/{tableId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTable(@PathVariable Integer restaurantId, @PathVariable Integer tableId) {
+        tableService.deleteTable(restaurantId, tableId);
     }
 }
---
+++ src/test/java/com/vytsablinskas/flavorfare/integration/controllers/TableControllerIntegrationTests.java
@@ -0,0 +1,358 @@
+package com.vytsablinskas.flavorfare.integration.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vytsablinskas.flavorfare.business.services.interfaces.RestaurantService;
+import com.vytsablinskas.flavorfare.business.services.interfaces.TableService;
+import com.vytsablinskas.flavorfare.shared.constants.Messages;
+import com.vytsablinskas.flavorfare.shared.dtos.restaurant.RestaurantDto;
+import com.vytsablinskas.flavorfare.shared.dtos.table.AddTableDto;
+import com.vytsablinskas.flavorfare.shared.dtos.table.TableDto;
+import com.vytsablinskas.flavorfare.shared.dtos.table.UpdateTableDto;
+import com.vytsablinskas.flavorfare.utils.RestaurantTestData;
+import com.vytsablinskas.flavorfare.utils.TableTestData;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
+public class TableControllerIntegrationTests {
+    private final MockMvc mockMvc;
+    private final TableService tableService;
+    private final RestaurantService restaurantService;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public TableControllerIntegrationTests(MockMvc mockMvc, TableService tableService, RestaurantService restaurantService, ObjectMapper objectMapper) {
+        this.mockMvc = mockMvc;
+        this.tableService = tableService;
+        this.restaurantService = restaurantService;
+        this.objectMapper = objectMapper;
+    }
+
+    @Test
+    public void getTables_validRestaurantId_returnsHttpStatus200() throws Exception {
+        RestaurantDto restaurantDto = restaurantService.addRestaurant(RestaurantTestData.getAddRestaurantDtoA());
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(String.format("/api/v1/restaurants/%d/tables", restaurantDto.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        );
+    }
+
+    @Test
+    public void getTables_invalidRestaurantId_returnsHttpStatus404WithMessage() throws Exception {
+        Integer invalidRestaurantId = 1;
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(String.format("/api/v1/restaurants/%d/tables", invalidRestaurantId))
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.status().isNotFound()
+        ).andExpect(
+                MockMvcResultMatchers.content().string(Messages.getRestaurantNotFoundMessage(invalidRestaurantId))
+        );
+    }
+
+    @Test
+    public void getTables_validRestaurantId_shouldReturnTableList() throws Exception {
+        RestaurantDto restaurantDto = restaurantService.addRestaurant(RestaurantTestData.getAddRestaurantDtoA());
+        TableDto table = tableService.addTable(restaurantDto.getId(), TableTestData.getAddTableDtoA());
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(String.format("/api/v1/restaurants/%d/tables", restaurantDto.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$[0].id").value(table.getId())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$[0].size").value(table.getSize())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$[0].count").value(table.getCount())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$[0].restaurantId").value(restaurantDto.getId())
+        );
+    }
+
+    @Test
+    public void getTable_validRestaurantId_returnsHttpStatus200() throws Exception {
+        RestaurantDto restaurantDto = restaurantService.addRestaurant(RestaurantTestData.getAddRestaurantDtoA());
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(String.format("/api/v1/restaurants/%d/tables", restaurantDto.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        );
+    }
+
+    @Test
+    public void getTable_invalidRestaurantId_returnsHttpStatus404WithMessage() throws Exception {
+        Integer invalidRestaurantId = 1;
+        Integer shouldNotReachTableId = 1;
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(String.format("/api/v1/restaurants/%d/tables/%d", invalidRestaurantId, shouldNotReachTableId))
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.status().isNotFound()
+        ).andExpect(
+                MockMvcResultMatchers.content().string(Messages.getRestaurantNotFoundMessage(invalidRestaurantId))
+        );
+    }
+
+    @Test
+    public void getTable_invalidTableId_returnsHttpStatus404WithMessage() throws Exception {
+        RestaurantDto restaurantDto = restaurantService.addRestaurant(RestaurantTestData.getAddRestaurantDtoA());
+        Integer invalidTableId = 1;
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(String.format("/api/v1/restaurants/%d/tables/%d", restaurantDto.getId(), invalidTableId))
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.status().isNotFound()
+        ).andExpect(
+                MockMvcResultMatchers.content().string(Messages.getTableNotFoundMessage(invalidTableId))
+        );
+    }
+
+    @Test
+    public void getTable_validRestaurantAndTableId_shouldReturnTableInformation() throws Exception {
+        RestaurantDto restaurantDto = restaurantService.addRestaurant(RestaurantTestData.getAddRestaurantDtoA());
+        TableDto table = tableService.addTable(restaurantDto.getId(), TableTestData.getAddTableDtoA());
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(String.format("/api/v1/restaurants/%d/tables/%d", restaurantDto.getId(), table.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.id").value(table.getId())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.size").value(table.getSize())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.count").value(table.getCount())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.restaurantId").value(restaurantDto.getId())
+        );
+    }
+
+    @Test
+    public void addTable_validRestaurantId_returnsHttpStatus201() throws Exception {
+        RestaurantDto restaurantDto = restaurantService.addRestaurant(RestaurantTestData.getAddRestaurantDtoA());
+        AddTableDto addTableDtoA = TableTestData.getAddTableDtoA();
+        String addTableDtoJson = objectMapper.writeValueAsString(addTableDtoA);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post(String.format("/api/v1/restaurants/%d/tables", restaurantDto.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(addTableDtoJson)
+        ).andExpect(
+                MockMvcResultMatchers.status().isCreated()
+        );
+    }
+
+    @Test
+    public void addTable_invalidRestaurantId_returnsHttpStatus404WithMessage() throws Exception {
+        Integer invalidRestaurantId = 1;
+        AddTableDto addTableDtoA = TableTestData.getAddTableDtoA();
+        String addTableDtoJson = objectMapper.writeValueAsString(addTableDtoA);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post(String.format("/api/v1/restaurants/%d/tables", invalidRestaurantId))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(addTableDtoJson)
+        ).andExpect(
+                MockMvcResultMatchers.status().isNotFound()
+        ).andExpect(
+                MockMvcResultMatchers.content().string(Messages.getRestaurantNotFoundMessage(invalidRestaurantId))
+        );
+    }
+
+    @Test
+    public void addTable_tableSizeAlreadyInDatabase_returnsHttpStatus422WithMessage() throws Exception {
+        RestaurantDto restaurantDto = restaurantService.addRestaurant(RestaurantTestData.getAddRestaurantDtoA());
+        AddTableDto addTableDtoA = TableTestData.getAddTableDtoA();
+        tableService.addTable(restaurantDto.getId(), addTableDtoA);
+        AddTableDto addTableDtoB = TableTestData.getAddTableDtoB();
+        addTableDtoB.setSize(addTableDtoA.getSize());
+        String addTableDtoJson = objectMapper.writeValueAsString(addTableDtoB);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post(String.format("/api/v1/restaurants/%d/tables", restaurantDto.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(addTableDtoJson)
+        ).andExpect(
+                MockMvcResultMatchers.status().isUnprocessableEntity()
+        ).andExpect(
+                MockMvcResultMatchers.content().string(Messages.getTableOfSizeIsDuplicateMessage(addTableDtoA.getSize()))
+        );
+    }
+
+    @Test
+    public void addTable_validRestaurantId_shouldAddTableAndReturnItsInformation() throws Exception {
+        RestaurantDto restaurantDto = restaurantService.addRestaurant(RestaurantTestData.getAddRestaurantDtoA());
+        AddTableDto addTableDtoA = TableTestData.getAddTableDtoA();
+        String addTableDtoJson = objectMapper.writeValueAsString(addTableDtoA);
+
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post(String.format("/api/v1/restaurants/%d/tables", restaurantDto.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(addTableDtoJson)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.id").isNumber()
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.size").value(addTableDtoA.getSize())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.count").value(addTableDtoA.getCount())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.restaurantId").value(restaurantDto.getId())
+        );
+    }
+
+    @Test
+    public void updateTable_validIds_returnsHttpStatus200() throws Exception {
+        RestaurantDto restaurantDto = restaurantService.addRestaurant(RestaurantTestData.getAddRestaurantDtoA());
+        TableDto table = tableService.addTable(restaurantDto.getId(), TableTestData.getAddTableDtoA());
+        UpdateTableDto updateTableDtoA = TableTestData.getUpdateTableDtoA();
+        String addTableDtoJson = objectMapper.writeValueAsString(updateTableDtoA);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put(String.format("/api/v1/restaurants/%d/tables/%d", restaurantDto.getId(), table.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(addTableDtoJson)
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        );
+    }
+
+    @Test
+    public void updateTable_invalidRestaurantId_returnsHttpStatus404WithMessage() throws Exception {
+        Integer invalidRestaurantId = 1;
+        Integer shouldNotReachTableId = 1;
+        UpdateTableDto updateTableDto = TableTestData.getUpdateTableDtoA();
+        String updateTableDtoJson = objectMapper.writeValueAsString(updateTableDto);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put(String.format("/api/v1/restaurants/%d/tables/%d", invalidRestaurantId, shouldNotReachTableId))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateTableDtoJson)
+        ).andExpect(
+                MockMvcResultMatchers.status().isNotFound()
+        ).andExpect(
+                MockMvcResultMatchers.content().string(Messages.getRestaurantNotFoundMessage(invalidRestaurantId))
+        );
+    }
+
+    @Test
+    public void updateTable_invalidTableId_returnsHttpStatus404WithMessage() throws Exception {
+        RestaurantDto restaurantDto = restaurantService.addRestaurant(RestaurantTestData.getAddRestaurantDtoA());
+        Integer invalidTableId = 1;
+        String updateTableDtoJson = objectMapper.writeValueAsString(TableTestData.getUpdateTableDtoA());
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put(String.format("/api/v1/restaurants/%d/tables/%d", restaurantDto.getId(), invalidTableId))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateTableDtoJson)
+        ).andExpect(
+                MockMvcResultMatchers.status().isNotFound()
+        ).andExpect(
+                MockMvcResultMatchers.content().string(Messages.getTableNotFoundMessage(invalidTableId))
+        );
+    }
+
+    @Test
+    public void updateTable_tableSizeAlreadyInDatabase_returnsHttpStatus422WithMessage() throws Exception {
+        RestaurantDto restaurantDto = restaurantService.addRestaurant(RestaurantTestData.getAddRestaurantDtoA());
+        TableDto table = tableService.addTable(restaurantDto.getId(), TableTestData.getAddTableDtoA());
+        TableDto table2 = tableService.addTable(restaurantDto.getId(), TableTestData.getAddTableDtoB());
+        UpdateTableDto updateTableDtoA = TableTestData.getUpdateTableDtoA();
+        updateTableDtoA.setSize(table.getSize());
+        String updateTableDtoJson = objectMapper.writeValueAsString(updateTableDtoA);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put(String.format("/api/v1/restaurants/%d/tables/%d", restaurantDto.getId(), table2.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateTableDtoJson)
+        ).andExpect(
+                MockMvcResultMatchers.status().isUnprocessableEntity()
+        ).andExpect(
+                MockMvcResultMatchers.content().string(Messages.getTableOfSizeIsDuplicateMessage(updateTableDtoA.getSize()))
+        );
+    }
+
+    @Test
+    public void updateTable_validIds_shouldUpdateTableAndReturnItsInformation() throws Exception {
+        RestaurantDto restaurantDto = restaurantService.addRestaurant(RestaurantTestData.getAddRestaurantDtoA());
+        TableDto table = tableService.addTable(restaurantDto.getId(), TableTestData.getAddTableDtoA());
+        UpdateTableDto updateTableDtoA = TableTestData.getUpdateTableDtoA();
+        String addTableDtoJson = objectMapper.writeValueAsString(updateTableDtoA);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put(String.format("/api/v1/restaurants/%d/tables/%d", restaurantDto.getId(), table.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(addTableDtoJson)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.id").value(table.getId())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.size").value(updateTableDtoA.getSize())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.count").value(updateTableDtoA.getCount())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.restaurantId").value(restaurantDto.getId())
+        );
+    }
+
+    @Test
+    public void deleteTable_validIds_returnsHttpStatus200() throws Exception {
+        RestaurantDto restaurantDto = restaurantService.addRestaurant(RestaurantTestData.getAddRestaurantDtoA());
+        TableDto table = tableService.addTable(restaurantDto.getId(), TableTestData.getAddTableDtoA());
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.delete(String.format("/api/v1/restaurants/%d/tables/%d", restaurantDto.getId(), table.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.status().isNoContent()
+        );
+    }
+
+    @Test
+    public void deleteTable_invalidRestaurantId_returnsHttpStatus404WithMessage() throws Exception {
+        Integer invalidRestaurantId = 1;
+        Integer shouldNotReachTableId = 1;
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.delete(String.format("/api/v1/restaurants/%d/tables/%d", invalidRestaurantId, shouldNotReachTableId))
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.status().isNotFound()
+        ).andExpect(
+                MockMvcResultMatchers.content().string(Messages.getRestaurantNotFoundMessage(invalidRestaurantId))
+        );
+    }
+
+    @Test
+    public void deleteTable_invalidTableId_returnsHttpStatus404WithMessage() throws Exception {
+        RestaurantDto restaurantDto = restaurantService.addRestaurant(RestaurantTestData.getAddRestaurantDtoA());
+        Integer invalidTableId = 1;
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.delete(String.format("/api/v1/restaurants/%d/tables/%d", restaurantDto.getId(), invalidTableId))
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.status().isNotFound()
+        ).andExpect(
+                MockMvcResultMatchers.content().string(Messages.getTableNotFoundMessage(invalidTableId))
+        );
+    }
+}
